57a6093705bc30fffa731bce990d6b5617b9613d
kimywann <kimywan10@gmail.com>
2025-09-03 21:56:42 +0900

    chore: 프로젝트 초기 설정 및 의존성 구성
    - Next.js 15.5.2 프로젝트 초기화
    - TanStack Query, Axios, Tailwind CSS 등 필수 의존성 추가
    - RAWG API 이미지 도메인 허용 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["fakestoreapi.com", "cdn.myanimelist.net", "media.rawg.io"],
+  },
 };
 
 export default nextConfig;
