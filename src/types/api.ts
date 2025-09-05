7f5c653f8c76c1336826f3cecc01365c519f6541
kimywann <kimywan10@gmail.com>
2025-09-05 10:11:27 +0900

    feat: 스크린샷 및 API 응답 타입 정의 추가

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,16 @@
 import { Game } from "./game";
+import { ScreenShot } from "./screen-shot";
 
 export interface ApiResponse {
   count: number;
   next?: string | null;
   previous?: string | null;
   results: Game[];
+}
+
+export interface ScreenShotResponse {
+  count: number;
+  next?: string | null;
+  previous?: string | null;
+  results: ScreenShot[];
 }
