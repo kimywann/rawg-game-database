0e78eceb4488355b2df19dd51eabab9e3ce44cc5
kimywann <kimywan10@gmail.com>
2025-10-04 22:41:08 +0900

    style: 스켈레톤 h 크기 조절

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -6,7 +6,7 @@ export const Skeleton = () => {
           key={i}
           className="w-full max-w-80 animate-pulse sm:w-[calc(50%-10px)] xl:w-[calc(33.333%-14px)] 2xl:w-[calc(25%-15px)] [@media(min-width:1800px)]:w-[calc(20%-16px)]"
         >
-          <div className="mb-3 h-60 w-full rounded-lg bg-zinc-700"></div>
+          <div className="mb-3 h-80 w-full rounded-lg bg-zinc-700"></div>
           <div className="mb-3 h-5 w-full rounded-lg bg-zinc-700"></div>
         </div>
       ))}
